9ee7a71b25ec337012e030f67be775c2286c673e
AndrewY97 <49506511+AndrewY97@users.noreply.github.com>
2020-06-04 02:12:01 -0500

    ORACLE

--- SistemaABCC/src/conexionBD/ConexionOracle.java
+++ SistemaABCC/src/conexionBD/ConexionOracle.java
@@ -1,4 +1,82 @@
 package conexionBD;
+import java.sql.*;
+import javax.swing.JOptionPane;
 
 public class ConexionOracle {
+    private Connection conexion;
+    //private Statement stm; //problema: permite SQL INJECTION
+
+    private PreparedStatement ps; //evita SQL INJECTION
+
+    private ResultSet rs;
+
+    private static Connection con=null;
+
+    private static Connection getConnection(){
+        try{
+            if( con == null ){
+                String driver="oracle.jdbc.OracleDriver"; //el driver varia segun la DB que usemos
+                String url="jdb:oracle:thin@localhost:1521:xe";
+                String pwd="Acevedoa97";
+                String usr="SYSTEM";
+                Class.forName(driver);
+                con = DriverManager.getConnection(url,usr,pwd);
+                System.out.println("Conectionesfull");
+            }
+        }
+        catch(ClassNotFoundException | SQLException ex){
+            ex.printStackTrace();
+        }
+        return con;
+    }
+
+
+    //Metodo para ejecutar instrucciones DDL y DML (Altas, Bajas y Cambios, entre otras)
+    public boolean ejecutarInstruccion(String sql) {
+        try {
+            Connection c = ConexionOracle.getConnection();
+            //PreparedStatement st;
+            ps = c.prepareStatement(sql);
+            int res = ps.executeUpdate(sql);
+            System.out.println("CONEXION BD: " + res);
+            return res==1 ? true : false;
+
+        } catch (SQLException throwables) {
+            System.out.println("Error en la instruccion SQL \n" + sql);
+            throwables.printStackTrace();
+            return false;
+        }
+
+    }
+
+    //Metodo para CONSULTAS (instrucciones SQL, por ejemplo SELECT * FROM ....)
+    public ResultSet ejecutarConsultaRegistros(String sql) {
+        ResultSet rs = null;
+        try {
+            Connection c = ConexionOracle.getConnection();
+            //PreparedStatement st;
+            ps = c.prepareStatement(sql);
+            return ps.executeQuery(sql);
+        } catch (SQLException throwables) {
+            System.out.println("Error en la instruccion SQL \n" + sql);
+            throwables.printStackTrace();
+            return  rs;
+        }
+    }
+
+    public void cerrarConexion(){
+        try {
+            ps.close();
+            conexion.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+	/*public static void main(String[] args) {
+		new ConexionBD();
+	}*/
+
+
 }
